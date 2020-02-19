80a4deabb44f65fa2c879c13e3ca1dda124bc549
Obed Nuertey <onuertey1997@gmail.com>
2020-02-19 01:01:00 +0000

    patch: on line 8 to 12 app.py

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ from config import NEWS_API_KEY
 
 # Create the Flask app
 app = Flask(__name__)
+
+# Define the home page route
+@app.route('/')
+def home():
+    query = request.args.get("query", "latest")
