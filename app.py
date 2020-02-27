a5f4dfabe232d7c7ffc5083ebad3792e8949d9d2
Obed Nuertey <onuertey1997@gmail.com>
2020-02-27 04:27:41 +0000

    patch: on line 14 app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def home():
     query = request.args.get("query", "latest")
     url = f"https://newsapi.org/v2/everything?q={query}&apiKey={NEWS_API_KEY}"
+    response = requests.get(url)
 
 if __name__ == "__main__":
     app.run(debug=True)
